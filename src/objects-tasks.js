ff16b5d795b02d2866de426bca98818715790981
RemAntof <anton.ramarchuk@gmail.com>
2024-03-07 21:10:49 +0100

    feat: add solution of first task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const obj2 = {};
+  return Object.assign(obj2, obj);
 }
 
 /**
@@ -32,8 +33,10 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((basket, obj) => {
+    return Object.assign(basket, Object.entries(obj));
+  }, {});
 }
 
 /**
